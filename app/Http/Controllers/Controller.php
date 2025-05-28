e7701ee59b90f98f5cc9e8f4a2c6b762df952530
jonas portugall <jonascportugal30@gmail.com>
2025-05-28 18:45:07 +0100

    swagger added to do documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,24 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    /**
+ * @OA\Info(
+ *    title="TO DO LIST  - DOCUMENTATION",
+ *    version="1.0.0",
+ *    description="API documentation ",
+ *    @OA\Contact(
+ *        email="jonascportugal30@gmail.com"
+ *    )
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="sanctum",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     in="header",
+ *     name="Authorization",
+ *     description="Use 'Bearer {token}' to authentication"
+ * )
+ */
 }
